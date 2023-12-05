258890b29c5228368321a4729470a6fb9b82ea4c
Gianni Schiralli <gschiralli@hotmail.com>
2023-12-05 18:01:54 -0500

    Add day 02

---
+++ src/02/index.js
@@ -0,0 +1,59 @@
+import { input } from "./input.js";
+
+const MAX_COUNT = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+//Part One
+function partOne() {
+  let lines = input.trim().split("\n");
+  const res = lines
+    .map((line) => {
+      return line
+        .split(": ")[1]
+        .split("; ")
+        .map((set) => {
+          const pulls = set.trim().split(", ");
+          return pulls.every((pull) => {
+            const [count, colour] = pull.split(" ");
+            return MAX_COUNT[colour] >= Number(count);
+          });
+        })
+        .every((p) => p);
+    })
+    .reduce((s, result, i) => {
+      return result ? s + (i + 1) : s;
+    });
+  console.log(res);
+}
+
+//Part Two
+export default function () {
+  console.log("--- Day 02 ---");
+  let lines = input.trim().split("\n");
+  const res = lines
+    .map((line) => {
+      let maxCount = {
+        red: 0,
+        green: 0,
+        blue: 0,
+      };
+      line
+        .split(": ")[1]
+        .split("; ")
+        .forEach((set) => {
+          const pulls = set.trim().split(", ");
+          pulls.forEach((pull) => {
+            const [count, colour] = pull.split(" ");
+            maxCount[colour] = Math.max(count, maxCount[colour]);
+          });
+        });
+      const { red, green, blue } = maxCount;
+      return red * green * blue;
+    })
+    .reduce((s, result) => s + result, 0);
+
+  console.log(`solution: ${res}`);
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import solve01 from "../src/01/index.js";
+import solve02 from "../src/02/index.js";
 
 function solveAll() {
   solve01();
+  solve02();
 }
 
 solveAll();
